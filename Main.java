16deada6ac7fefd7f272de9d7a8e98db9501bc09
Martin Ayvazyan <martinayvazyan@workfront.com>
2018-07-10 17:14:56 +0400

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+  class Main{
+	
+	public static void main(String... args){
+	  System.out.println(fibonacci(4));	
+  	}  
+
+       	static int fibonacci(int n) {
+         
+	     if(n<=1) return n;
+	   
+	     return fibonacci(n-1) + fibonacci(n-2);
+	   
+ 		
+         }
+
+	
+}
